be5a1732bd47bfe0c1eaf8c1c4d25433cb80c1ad
juan <juanjo_070193@hotmail.com>
2013-04-05 16:15:21 -0600

    Mezcla de archivos

---
+++ archivos/archivos/Principal.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace archivos
+{
+	class Principal
+	{
+		public static void Main (string[] args)
+		{
+
+			Archivo archivo = new Archivo("tres.txt");
+			archivo.Mezclar();
+
+
+
+			
+
+		}
+	}
+
+}
+
